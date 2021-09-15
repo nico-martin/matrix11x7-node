a8b82147bccfa6d271814684b73626b469422c79
Nico Martin <mail@nicomartin.ch>
2021-09-15 22:31:57 +0200

    added examples for fonts

--- examples/scroll2.js
+++ examples/scroll2.js
@@ -1,11 +1,13 @@
 const DisplayController = require("../src/display");
 const fontScroller = require("../src/scrolling");
+const font = require("../src/fonts/font5x7smoothed");
 
 const init = async () => {
   try {
     const display = await DisplayController();
     fontScroller(display, "Hello World!", {
       intensity: 80,
+      font,
     });
   } catch (e) {
     console.error(e);
