568e4573c644d4ad66ea15857b4e000af77029fd
Nico Martin <mail@nicomartin.ch>
2021-09-15 23:02:12 +0200

    added character check

--- src/font.js
+++ src/font.js
@@ -7,11 +7,7 @@ module.exports = fontToMatrix = (text, font = defaultFont) => {
   text.split("").map((char) => {
     const code = char.charCodeAt(0);
     const characterMatrix = font.characters[code];
-    matrix = matrix.map((row, index) =>
-      characterMatrix.indexOf(index) !== -1
-        ? [...row, 0, ...characterMatrix[index]]
-        : console.error(index + " not found")
-    );
+    matrix = matrix.map((row, index) => [...row, 0, ...characterMatrix[index]]);
   });
 
   if (font.height === 5) {
