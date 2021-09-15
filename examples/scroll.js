ebcc00b949b47e24937fcf40dcdf424579c3bfd6
Nico Martin <mail@nicomartin.ch>
2021-09-15 22:19:02 +0200

    changed speed

--- examples/scroll.js
+++ examples/scroll.js
@@ -4,7 +4,7 @@ const fontScroller = require("../src/scrolling");
 const init = async () => {
   try {
     const display = await DisplayController();
-    fontScroller(display, "Lorem ipsum", 50);
+    fontScroller(display, "Hello World!");
   } catch (e) {
     console.error(e);
   }
