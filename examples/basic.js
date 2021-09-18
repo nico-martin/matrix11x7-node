8ec09780ff7776fe83ed9827c539b99f69d6c101
Nico Martin <mail@nicomartin.ch>
2021-09-18 22:07:34 +0200

    bugfixing

--- examples/basic.js
+++ examples/basic.js
@@ -1,4 +1,5 @@
 const matrix11x7 = require("../index");
+const { wait } = require("../src/helpers");
 
 const matrix = [
   [0, 0, 0, 0, 50, 0, 50, 0, 0, 0, 0],
@@ -10,10 +11,27 @@ const matrix = [
   [0, 0, 0, 0, 0, 50, 0, 0, 0, 0, 0],
 ];
 
+const matrixClear = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+];
+
 const init = async () => {
   try {
     const instance = await matrix11x7();
     instance.basicMatrix(matrix);
+    await wait(1000);
+    instance.basicMatrix(matrixClear);
+    await wait(1000);
+    instance.basicMatrix(matrix);
+    await wait(1000);
+    instance.basicMatrix(matrixClear);
+    await wait(1000);
   } catch (e) {
     console.error(e);
   }
