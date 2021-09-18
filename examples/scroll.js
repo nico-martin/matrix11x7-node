db60e67ae9307f0d84eede2b9320055669104cd6
Nico Martin <mail@nicomartin.ch>
2021-09-18 22:33:38 +0200

    some change

--- examples/scroll.js
+++ examples/scroll.js
@@ -5,11 +5,10 @@ const init = async () => {
     const instance = await matrix11x7();
     await instance.scrollText("Hello World", {
       speed: 80, // optional - milliseconds between each step
-      infinite: false, // optional - wether it should stop after one run or keep running
+      infinite: true, // optional - wether it should stop after one run or keep running
       font: null, // optional - the font you want to use (see src/fonts/ as an example)
       intensity: 50, // optional - an integer between 0 an 255 (intensity)
     });
-    console.log("done");
   } catch (e) {
     console.error(e);
   }
